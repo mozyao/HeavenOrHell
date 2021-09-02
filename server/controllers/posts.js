6efcb6c58337b8e02cdf09ffb351e2a1ca104ef4
mozyao <yaomozzie@gmail.com>
2021-09-02 16:15:28 -0700

    Added controller+models, Connected with mongoDB

---
+++ server/controllers/posts.js
@@ -0,0 +1,8 @@
+export const getPosts= (req,res) => {
+    res.send("this works"); // call back function is executed when someone visits localhost3000/
+}
+
+export const createPost= (req,res) => {
+    res.send("post created"); // call back function is executed when someone visits localhost3000/
+}
+
--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
- 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+app.use('/posts',postRoutes);
+app.use(express.json({limit:"30mb",extended:true}));
+app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://gigas:Ji9hMLwfTfcjg89@cluster0.qqzdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT,()=> console.log(`Server on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
