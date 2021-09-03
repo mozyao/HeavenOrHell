9943d6f72454531dd40982e3db940200c5730ea9
mozyao <yaomozzie@gmail.com>
2021-09-02 21:51:54 -0700

    Set up the clients Post and Form structure, initial H/H front page connected successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,45 @@
- import React from 'react';
+import React from 'react';
+import {Container, AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import memories from './imgs/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+
+
 
  const App= () =>{
     return (
+        // make it large
+        <Container maxidth="lg"> 
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">H/H</Typography>
+                <img src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing ={3}>
+
+                    </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
 
 
-        <div>
-            <h1> Heaven or Hell</h1>
-        </div>
+                           
+                     
 
+
+                </Container>
+            </Grow>
+
+
+
+
+        </Container>
 
     );
  }
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Post=() => {
+
+    return (
+        <h1> Post</h1>
+        
+        
+        
+        );
+
+}
+
+export default Post;
