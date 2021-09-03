aa250ebaa6a780033ee0085b2d3a49dd4170ffae
mozyao <yaomozzie@gmail.com>
2021-09-02 17:17:09 -0700

    getPosts and createPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,29 @@
-export const getPosts= (req,res) => {
-    res.send("this works"); // call back function is executed when someone visits localhost3000/
+ 
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts= async (req,res) => {
+    try{
+        //retrieve all posts, while we may want to wait since 
+        //each retrieve process takes time
+        const postMessages= await PostMessage.find();
+        //console.log(postMessages);
+        res.status(200).json(postMessages);
+    }
+    catch(error){
+        res.status(404).json({message: error.message});
+    } 
 }
 
-export const createPost= (req,res) => {
-    res.send("post created"); // call back function is executed when someone visits localhost3000/
+export const createPost= async (req,res) => {
+    // res.send("post created"); 
+    const post=req.body;
+    const newPost = new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    }catch(error){
+        res.status(409).json({message: error.message});
+    }
 }
 
