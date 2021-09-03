f372934c672d6a0f7d3666c60f55dc3783d6fc71
mozyao <yaomozzie@gmail.com>
2021-09-02 22:09:40 -0700

    Fixed the UI efforts on diff screen size

--- client/src/App.js
+++ client/src/App.js
@@ -3,41 +3,35 @@ import {Container, AppBar,Typography,Grow,Grid} from '@material-ui/core';
 import memories from './imgs/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 
 
 
  const App= () =>{
+    const classes = useStyles();
     return (
         // make it large
         <Container maxidth="lg"> 
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">H/H</Typography>
-                <img src={memories} alt="memories" height="60"></img>
+            <AppBar className ={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">H/H</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"></img>
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justifyContent="space-between" alignItems="stretch" spacing ={3}>
 
-                    </Grid>
+                    
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
 
-                        <Grid item xs={12} sm={7}>
+                        <Grid item xs={12} sm={4}>
                             <Form />
                         </Grid>
-
-
-                           
-                     
-
-
+                    </Grid>
                 </Container>
             </Grow>
-
-
-
 
         </Container>
 
