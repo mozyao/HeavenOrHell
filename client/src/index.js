b2e1a5b85d42218158731588ef67304eb09f3a6c
mozyao <yaomozzie@gmail.com>
2021-09-04 11:09:24 -0700

    Bug fixed - set the ip of mongoDB cluster to be accesed from anyway+file conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider} from 'react-redux';
-import { createStore, applyMiddleware,compose} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
 
+import { reducers } from './reducers';
 import App from './App';
-const store = createStore( reducers, compose(applyMiddleware(thunk)));
+import './index.css';
 
-ReactDOM.render( 
-    <Provider store={store}>
-        <App />
-    </Provider>,
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-    document.getElementById('root')); // connect to the div with an id of root
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use(express.json({limit:"30mb",extended:true}));
-app.use(express.urlencoded({limit:"30mb",extended:true}));
+dotenv.config();
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use('/posts',postRoutes);
-const CONNECTION_URL = 'mongodb+srv://gigas:Ji9hMLwfTfcjg89@cluster0.qqzdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5001;
+app.use('/posts', postRoutes);
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
-    .then(()=> app.listen(PORT,()=> console.log(`Server on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+app.get('/',(req, res) => {
+  res.send('Welcome to HoH API');
+
+});
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
